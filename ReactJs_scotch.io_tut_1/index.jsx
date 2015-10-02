b3451402d48508b28e769ee8af8e6592adbfc6d8
niksolaz <solazzo.nicola@gmail.com>
2015-10-02 18:13:33 +0200

    create ancor with social

--- ReactJs_scotch.io_tut_1/index.jsx
+++ ReactJs_scotch.io_tut_1/index.jsx
@@ -1,4 +1,4 @@
-/*var MyComponents = React.createClass({
+var MyComponents = React.createClass({
           render: function(){
             return (
 
@@ -7,8 +7,12 @@
           }
       });
 
-      React.render(
-          <div>
-                <MyComponents name="Nik Solaz" />
-          </div>,
-          document.getElementById('content'));*/
+React.render(
+    <div>
+          <MyComponents name="Facebook"><a href="http://www.facebook.com">Facebook</a></MyComponents>
+          <MyComponents name="Twitter"><a href="http://www.twitter.com">Twitter</a></MyComponents>
+          <MyComponents name="Google+"><a href="http://www.googleplus.com">Google Plus</a></MyComponents>
+    </div>,
+    document.getElementById('content'));
+
+module.exports= index;
