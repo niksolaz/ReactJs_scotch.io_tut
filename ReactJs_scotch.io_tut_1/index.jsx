40a29e62f39a09cbeac6207b2a68558e016a9480
niksolaz <solazzo.nicola@gmail.com>
2015-10-02 19:21:59 +0200

    button refresh

--- ReactJs_scotch.io_tut_1/index.jsx
+++ ReactJs_scotch.io_tut_1/index.jsx
@@ -1,14 +1,15 @@
 var MyComponents = React.createClass({
 
           doSomething: function(){
-            <button>{this.props.children}</button>
+              
+            
           },
 
           render: function(){
             return (
                     <div>
                           <h1>{this.props.name}</h1>
-                          <input type="button" id="myBtn" onClick={this.doSomething}>{this.props.social}</input>
+                          <input type="submit"><a onClick={this.doSomething}>{this.props.social}</a></input>
                           
                     </div>
                     
@@ -19,10 +20,14 @@ var MyComponents = React.createClass({
 React.render(
 
     <div>
+      <div>
           <MyComponents name="Nik Solaz" />
-          <MyComponents social="Facebook"><a href="http://www.facebook.com"></a></MyComponents>
-          <MyComponents social="Twitter"><a href="http://www.twitter.com"></a></MyComponents>
-          <MyComponents social="GooglePlus"><a href="http://www.googleplus.com"></a></MyComponents>
+      </div>
+      <div>
+          <MyComponents social="Facebook"></MyComponents>
+          <MyComponents social="Twitter"></MyComponents>
+          <MyComponents social="GooglePlus"></MyComponents>
+      </div>
     </div>,
     document.getElementById('content'));
 
