f25749643221de246fcb079a5900ed38c26784fa
niksolaz <solazzo.nicola@gmail.com>
2015-10-02 18:35:58 +0200

    create element JSX

--- ReactJs_scotch.io_tut_1/index.jsx
+++ ReactJs_scotch.io_tut_1/index.jsx
@@ -1,17 +1,30 @@
 var MyComponents = React.createClass({
+
+          doSomething: function(){
+            return(
+                  <button>{this.props.children}</button>
+            );
+          },
+
           render: function(){
             return (
-
-                    <h1>Welcome {this.props.name}!</h1>
+                    <div>
+                          <h1>Welcome {this.props.name}!</h1>
+                          <a onClick={this.doSomething}>{this.props.social}</a>
+                          
+                    </div>
+                    
             );
           }
       });
 
 React.render(
+
     <div>
-          <MyComponents name="Facebook"><a href="http://www.facebook.com">Facebook</a></MyComponents>
-          <MyComponents name="Twitter"><a href="http://www.twitter.com">Twitter</a></MyComponents>
-          <MyComponents name="Google+"><a href="http://www.googleplus.com">Google Plus</a></MyComponents>
+          <MyComponents name="Nik Solaz" />
+          <MyComponents social="Facebook"><a href="http://www.facebook.com"></a></MyComponents>
+          <MyComponents social="Twitter"><a href="http://www.twitter.com"></a></MyComponents>
+          <MyComponents social="GooglePlus"><a href="http://www.googleplus.com"></a></MyComponents>
     </div>,
     document.getElementById('content'));
 
