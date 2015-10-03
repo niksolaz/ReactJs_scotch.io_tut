3ba39d6996c32bbbcfff74475b556b9bc6b9b915
niksolaz <solazzo.nicola@gmail.com>
2015-10-03 16:30:45 +0200

    components header

--- ReactJs_scotch.io_tut_1/index.jsx
+++ ReactJs_scotch.io_tut_1/index.jsx
@@ -1,19 +1,8 @@
-var MyComponents = React.createClass({
-
-          doSomething: function(){
-            return (
-              <div>
-                    <p>{this.props.children}</p>
-              </div>
-              );
-          },
-
+var Header = React.createClass({
           render: function(){
             return (
-                    <div>
-                          <h1>{this.props.name}</h1>
-                          <input type="submit"><a onClick={this.doSomething}>Send to this {this.props.social}</a></input>
-                          
+                    <div> 
+                          <h3> SMSL of {this.props.name}</h3>      
                     </div>
                     
             );
@@ -23,16 +12,14 @@ var MyComponents = React.createClass({
 React.render(
 
     <div>
-      <div>
-          <MyComponents name="Nik Solaz" />
-          <p>all social</p>
-      </div>
-      <div>
-          <MyComponents social="Facebook"><a href="http://www.facebook.com"></a></MyComponents>
-          <MyComponents social="Twitter"><a href="http://www.twitter.com"></a></MyComponents>
-          <MyComponents social="GooglePlus"><a href="http://www.googleplus.com"></a></MyComponents>
-      </div>
+          <Header name="Nik Solaz" /> 
     </div>,
     document.getElementById('content'));
 
 module.exports= index;
+
+
+
+
+
+//<input type="submit"><a onClick={this.doSomething}>Send to this {this.props.social}</a></input>
