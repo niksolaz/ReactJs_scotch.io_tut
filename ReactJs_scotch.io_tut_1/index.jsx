9bc898d755c8b8ef46181d655e923251c67135b3
niksolaz <solazzo.nicola@gmail.com>
2015-10-03 17:52:28 +0200

    uniq elemnt message text

--- ReactJs_scotch.io_tut_1/index.jsx
+++ ReactJs_scotch.io_tut_1/index.jsx
@@ -14,16 +14,24 @@ var ButtonSocial = React.createClass({
             {this.props.social}
             </button>
             
+            
       );
     }
 });
 
 React.render(
     <div>
-    <NameOfHeader name="Nik Solaz" />
-    <ButtonSocial social="Facebook" site="http://www.facebook.com" />
-    <ButtonSocial social="Twitter" site="http://www.twitter.com" />
-    <ButtonSocial social="Google Plus" site="http://www.googleplus.com" />
+        <NameOfHeader name="Nik Solaz" />
+        <form>
+            <br />
+            <label>Insert your text</label>
+            <br />
+            <textarea row="4" col="50"></textarea>
+            <br />
+            <ButtonSocial social="Facebook" site="http://www.facebook.com" />
+            <ButtonSocial social="Twitter" site="http://www.twitter.com" />
+            <ButtonSocial social="Google Plus" site="http://www.googleplus.com" />
+        </form>
     </div>,
     document.getElementById('content'));
 
